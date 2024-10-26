079feaa8a3f6f17be75f395b9589f95323ec4828
godsvra <jjmsvra@gmail.com>
2024-10-26 23:43:09 +0900

    cleaning up the text file. Fixed.

---
+++ SpinFIle.py
@@ -0,0 +1,16 @@
+#A4 Word Spinner
+#Participant: Jiho Im (solo)
+#GitHub repo link:
+
+import string
+def main():
+    # read the text file
+    with open("short_text.txt") as file:
+        original_text = file.read()
+    #make all letters lower case and remove punctuations
+    original_text_cleaned = original_text.lower().translate(str.maketrans("", "", string.punctuation))
+    print (original_text_cleaned)
+
+
+if __name__ == "__main__":
+    main()
